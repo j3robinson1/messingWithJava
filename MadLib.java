fa2637207de8755aa04e3fc34e069400c282af4d
Jesse Robinson <j3robinson1@gmail.com>
2015-04-14 14:11:16 -0400

    madlib

---
+++ MadLib.java
@@ -0,0 +1,14 @@
+import java.io.Console
+public class MadLib {
+
+  public static void main(String[] args) {
+    Console console = system.console();
+
+    String name = console.readLine("Enter your name:  ");
+    String adjective = console.readLine("Enter an adjective:  ");
+    String noun = console.readLine("Enter a noun:  ");
+    String adverb = console.readLine("Enter a adverb:  ");
+    String verb = console.readLine("Enter a verb ending in -ing:  ");
+    console.printf("%s is a %s %s. They are always %s %s/n", name, adjective, noun, adverb, verb);
+  }
+}
