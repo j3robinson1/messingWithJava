b7f95c66346897cfe7419b3ba0b1d8173ce3112f
Jesse Robinson <j3robinson1@gmail.com>
2015-04-14 14:22:54 -0400

    fixed some things, added age requirement

--- MadLib.java
+++ MadLib.java
@@ -1,14 +1,19 @@
-import java.io.Console
+import java.io.Console;
 public class MadLib {
 
   public static void main(String[] args) {
-    Console console = system.console();
-
+    Console console = System.console();
+    String ageAsString = console.readLine("Enter your age:  ");
+    int age = Integer.parseInt(ageAsString);
+    if (age < 13) {
+      console.printf("Sorry you must be atleast 13 to use this program.\n");
+      System.exit(0);
+    }
     String name = console.readLine("Enter your name:  ");
     String adjective = console.readLine("Enter an adjective:  ");
     String noun = console.readLine("Enter a noun:  ");
     String adverb = console.readLine("Enter a adverb:  ");
     String verb = console.readLine("Enter a verb ending in -ing:  ");
-    console.printf("%s is a %s %s. They are always %s %s/n", name, adjective, noun, adverb, verb);
+    console.printf("%s is a %s %s. They are always %s %s\n", name, adjective, noun, adverb, verb);
   }
 }
